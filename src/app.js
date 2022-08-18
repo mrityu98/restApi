c17c8835a076abb7b07833432f673b5e4dcd206f
Mrityunjay <mkctriangle@yahoo.in>
2022-08-18 11:31:06 +0530

    third

--- src/app.js
+++ src/app.js
@@ -1,19 +1,20 @@
-import express from 'express';
+import express, { urlencoded } from 'express';
 import dotenv from 'dotenv';
 import Connection from './db/conn.js';
 import mongoose  from 'mongoose';
 import Populations from './models/schema.js';
 import router from './router/router.js';
 import cors from 'cors';
-
+ 
 const app=express();
-const port = process.env.port || 6967;
+const port = process.env.PORT || 6967;
 dotenv.config();
 const USER=process.env.DB_USERNAME;
 const PASSWORD = process.env.DB_PASSWORD;
 
 app.use(express.json());
 app.use(cors())
+app.use(express.urlencoded({extended:true}))
 app.use(router);
 
 Connection(USER,PASSWORD);
